76806db9ec8f38c3d06471ba092bdc85ad09af4d
Andrew Njoo <adnjoo@gmail.com>
2023-11-07 06:47:25 -0800

    9: try e d ..

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
-import path from 'path';
+import path from "path";
 
 import { EXTENSIONS } from "./types";
-import { countLinesOfCode } from './functions';
+import { countLinesOfCode } from "./functions";
 
-const exclude = ['node_modules', '.next', '.git', '.vscode', '.vercel']; // TODO: https://github.com/adnjoo/slocjs/issues/4
+const exclude = ["node_modules", ".next", ".git", ".vscode", ".vercel"]; // TODO: https://github.com/adnjoo/slocjs/issues/4
 const rootPath = process.cwd();
-const srcPath = path.join(rootPath, 'src'); // TODO: https://github.com/adnjoo/slocjs/issues/2
+const srcPath = path.join(rootPath, "src"); // TODO: https://github.com/adnjoo/slocjs/issues/2
 const extensionsToCount = [EXTENSIONS.JS, EXTENSIONS.TS, EXTENSIONS.TSX]; // TODO: https://github.com/adnjoo/slocjs/issues/5
 const linesOfCode = countLinesOfCode(srcPath, extensionsToCount);
 
-// 
+//
 /**
  * Returns number of lines of code in a directory
- * @param dirPath 
+ * @param dirPath
  * @returns string
  */
 export function countLinesInDir(dirPath: string): string {
@@ -40,3 +40,7 @@ export function countLinesInDir(dirPath: string): string {
 //     console.log(`Updated ${readmeFile} with SLOC: ${linesOfCode}`);
 //   });
 // });
+
+export default {
+  countLinesInDir,
+};
