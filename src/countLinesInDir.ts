cff7266b0ecb94991afbf97a86c578f5da3cc5eb
Andrew Njoo <adnjoo@gmail.com>
2023-11-07 19:56:30 -0800

    cjs, esm

---
+++ src/countLinesInDir.ts
@@ -0,0 +1,21 @@
+import path from "path";
+
+import { EXTENSIONS } from "./types";
+import { countLinesOfCode } from "./countLinesOfCode";
+
+const exclude = ["node_modules", ".next", ".git", ".vscode", ".vercel"]; // TODO: https://github.com/adnjoo/slocjs/issues/4
+const rootPath = process.cwd();
+const srcPath = path.join(rootPath, "src"); // TODO: https://github.com/adnjoo/slocjs/issues/2
+const extensionsToCount = [EXTENSIONS.JS, EXTENSIONS.TS, EXTENSIONS.TSX]; // TODO: https://github.com/adnjoo/slocjs/issues/5
+const linesOfCode = countLinesOfCode(srcPath, extensionsToCount);
+
+/**
+ * Returns number of lines of code in a directory
+ * @param dirPath
+ * @returns string
+ */
+export function countLinesInDir(dirPath: string): string {
+  const extensionsToCount = [EXTENSIONS.JS, EXTENSIONS.TS, EXTENSIONS.TSX]; // TODO: https://github.com/adnjoo/slocjs/issues/5
+  const linesOfCode = countLinesOfCode(dirPath, extensionsToCount);
+  return `SLOC: ${linesOfCode}`;
+};
