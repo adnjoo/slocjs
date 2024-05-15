9b1680377c360194ebdbc5fb21e4df9ee027fee5
Andrew Njoo <adnjoo@gmail.com>
2024-05-15 02:38:21 -0700

    publish script

--- lib/cjs/countLinesInDir.d.ts
+++ lib/cjs/countLinesInDir.d.ts
@@ -1,6 +1,7 @@
+import { EXTENSIONS } from "./types";
 /**
  * Returns number of lines of code in a directory
  * @param dirPath
  * @returns string
  */
-export declare function countLinesInDir(dirPath: string): string;
+export declare function countLinesInDir(dirPath: string, extensions?: EXTENSIONS[], exclude?: string[]): string;
